09e299c12727feae6afe48afda5dc1d3527ccd45
Nick P <nicholas.pagano1982@gmail.com>
2022-04-04 18:27:04 +1000

    docs fix

--- vite.docs.config.ts
+++ vite.docs.config.ts
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   build: {
-    outDir: "docs",
+    assetsDir: "./assets",
+    outDir: "./docs",
     rollupOptions: {
+      makeAbsoluteExternalsRelative: true,
       input: {
         main: "./index.html",
       }
