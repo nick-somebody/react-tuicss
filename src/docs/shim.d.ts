ff8f01a5ce6c50fbf5e1ce30939e5f17141084b0
Nick P <nicholas.pagano1982@gmail.com>
2022-05-03 20:27:26 +1000

    added deps for docs site

---
+++ src/docs/shim.d.ts
@@ -0,0 +1,7 @@
+declare module 'https://cdn.jsdelivr.net/npm/react-router-dom/+esm' {
+  export * from "react-router-dom";
+}
+
+declare module 'https://esm.run/react-router-dom' {
+  export * from "react-router-dom";
+}
